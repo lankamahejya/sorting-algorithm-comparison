709266a51334cb824f9abb55e077442d31f915d2
chintharishithkumar <chintha.rishithkumar@klh.edu.in>
2026-06-10 12:16:35 +0530

    Added execution time comparison chart

--- static/script.js
+++ static/script.js
@@ -1,27 +1,33 @@
+let chart;
+
 async function compareSorts() {
 
     let input = document.getElementById("numbers").value;
 
     let values = input.split(",");
 
-let numbers = [];
+    let numbers = [];
 
-for(let value of values){
+    for(let value of values){
 
-    value = value.trim();
+        value = value.trim();
 
-    if(value === "" || isNaN(value)){
-        alert("Please enter valid numbers separated by commas.");
-        return;
+        if(value === "" || isNaN(value)){
+            alert("Please enter valid numbers separated by commas.");
+            return;
+        }
+
+        numbers.push(Number(value));
     }
 
-    numbers.push(Number(value));
-}
     const response = await fetch("/compare", {
+
         method: "POST",
+
         headers: {
             "Content-Type": "application/json"
         },
+
         body: JSON.stringify({
             numbers: numbers
         })
@@ -29,21 +35,54 @@ for(let value of values){
 
     const data = await response.json();
 
-document.getElementById("result").innerHTML = `
-    <h2>Results</h2>
+    document.getElementById("result").innerHTML = `
+        <h2>Results</h2>
 
-    <p><strong>Sorted Array:</strong> ${data.sorted}</p>
+        <p><strong>Sorted Array:</strong>
+        ${data.sorted}</p>
 
-    <p><strong>Bubble Sort Time:</strong>
-    ${data.bubble_time.toFixed(8)} seconds</p>
+        <p><strong>Bubble Sort Time:</strong>
+        ${data.bubble_time.toFixed(8)} seconds</p>
 
-    <p><strong>Merge Sort Time:</strong>
-    ${data.merge_time.toFixed(8)} seconds</p>
+        <p><strong>Merge Sort Time:</strong>
+        ${data.merge_time.toFixed(8)} seconds</p>
 
-    <p><strong>Bubble Sort Complexity:</strong>
-    ${data.bubble_complexity}</p>
+        <p><strong>Bubble Sort Complexity:</strong>
+        ${data.bubble_complexity}</p>
 
-    <p><strong>Merge Sort Complexity:</strong>
-    ${data.merge_complexity}</p>
-`;
+        <p><strong>Merge Sort Complexity:</strong>
+        ${data.merge_complexity}</p>
+    `;
+
+    const ctx = document.getElementById("timeChart");
+
+    if(chart){
+        chart.destroy();
+    }
+
+    chart = new Chart(ctx, {
+        type: "bar",
+
+        data: {
+            labels: ["Bubble Sort", "Merge Sort"],
+
+            datasets: [{
+                label: "Execution Time (seconds)",
+
+                data: [
+                    data.bubble_time,
+                    data.merge_time
+                ]
+            }]
+        },
+
+        options: {
+            responsive: true,
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
 }
